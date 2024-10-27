7cab9c6b3ba248fcc87c4456007653434fc5c394
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-10-27 19:58:27 +0900

    edit: 페이지컴포넌트 대문자로 변경

--- src/app/(mobile)/challenge/page.tsx
+++ src/app/(mobile)/challenge/page.tsx
@@ -1,5 +1,19 @@
-const page = () => {
+"use client";
+
+import { useEffect } from "react";
+
+const ChallengePage = () => {
+  useEffect(() => {
+    getWordData();
+  }, []);
+  const getWordData = async () => {
+    const data = await fetch(`/api/krdict`);
+    const result = await data?.json();
+    console.log("result", result);
+    return result;
+  };
+
   return <div>page</div>;
 };
 
-export default page;
+export default ChallengePage;
