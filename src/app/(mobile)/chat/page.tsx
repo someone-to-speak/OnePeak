c95a37b49ea2f1204610653cb43db2d37d46fda0
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-10-29 20:26:47 +0900

    feat: AI 코칭 프롬프트

--- src/app/(mobile)/chat/page.tsx
+++ src/app/(mobile)/chat/page.tsx
@@ -1,6 +1,35 @@
 import OpenAI from "openai";
 import React from "react";
 
+const openai = new OpenAI({
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY
+});
+
+const Page = async () => {
+  const text = "나 갈게 너집안으로";
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are a helpful Korean and English tutor who will help you improve your spoken language so that you can express yourself more naturally. If the user speaks in Korean, put the expression to correct in Korean in '' and give advice in English. If the user speaks in English, put the expression to be corrected in English in '' and give advice in Korean."
+      },
+      {
+        role: "user",
+        content: `${text}`
+      }
+    ]
+  });
+
+  const result = completion.choices[0].message.content;
+  // console.log(completion.choices[0].message);
+
+  return <>{result}</>;
+};
+
+export default Page;
+
 // const getAIData = async () => {
 //   const data = await fetch(`/api/getSuggestion`);
 //   const result = await data?.json();
@@ -8,28 +37,3 @@ import React from "react";
 //   return result;
 // };
 // getAIData();
-
-const openai = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY
-});
-
-const Page = async () => {
-  const completion = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
-    messages: [
-      {
-        role: "system",
-        content: "You are a helpful assistant that improves phrasing for spoken language to make it sound more natural."
-      },
-      {
-        role: "user",
-        content: "나를 장난감 사라졌어"
-      }
-    ]
-  });
-
-  console.log(completion.choices[0].message);
-  return <div>page</div>;
-};
-
-export default Page;
