e5001d4bfcd8a88e383ee45fb4c1acc9112ffdf9
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-10-29 17:22:08 +0900

    move: AI 호출을 chat 페이지로 이동

--- src/app/(mobile)/chat/page.tsx
+++ src/app/(mobile)/chat/page.tsx
@@ -1,5 +1,35 @@
-const page = () => {
+import OpenAI from "openai";
+import React from "react";
+
+// const getAIData = async () => {
+//   const data = await fetch(`/api/getSuggestion`);
+//   const result = await data?.json();
+//   console.log("result", result);
+//   return result;
+// };
+// getAIData();
+
+const openai = new OpenAI({
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY
+});
+
+const Page = async () => {
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant that improves phrasing for spoken language to make it sound more natural."
+      },
+      {
+        role: "user",
+        content: "나를 장난감 사라졌어"
+      }
+    ]
+  });
+
+  console.log(completion.choices[0].message);
   return <div>page</div>;
 };
 
-export default page;
+export default Page;
