228e441e16236a0b2e7e2cbca5d492111d802a0a
dajung <kwonkkan@naver.com>
2024-10-31 00:17:23 +0900

    work: 복습하기 데이터 불러오기 작업 중

--- src/components/chatBot/aiTutorHome/Reviewing.tsx
+++ src/components/chatBot/aiTutorHome/Reviewing.tsx
@@ -1,5 +1,52 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
+import { Tables } from "../../../../database.types";
+
+type ReviewType = Tables<"review">;
+
 const Reviewing = () => {
-  return <div>복습하기</div>;
+  const supabase = createClient();
+  const [reviews, setReviews] = useState<ReviewType[]>([]);
+
+  // TODO: 내 id랑 같은 것만 가져오기
+  const getReview = async () => {
+    try {
+      const { data, error } = await supabase.from("review").select("*");
+
+      if (error) {
+        throw error;
+      }
+
+      if (data) {
+        setReviews(data);
+      }
+    } catch (error) {
+      console.log("reviews를 가져오는 데에 실패하였습니다!", error);
+    }
+  };
+
+  useEffect(() => {
+    getReview();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">복습하기</h1>
+      <div>
+        {reviews.map((review) => {
+          return (
+            <div key={review.id}>
+              <p>{review.situation}</p>
+              <p>{review.level}</p>
+              <button>복습하기</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Reviewing;
