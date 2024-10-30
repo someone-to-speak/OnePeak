8bf52b169674488dc0f4607bcc4d364f3d63ae51
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-10-31 01:48:41 +0900

    work: user_answer, questions 데이터 불러오기

--- src/app/(mobile)/challenge/word/wrongAnswerNote/page.tsx
+++ src/app/(mobile)/challenge/word/wrongAnswerNote/page.tsx
@@ -14,11 +14,33 @@ interface UserAnswer {
   is_reviewed: boolean;
   created_at: Date;
 }
+interface Questions {
+  id: UUID;
+  type: string;
+  content: string;
+  question_id: number;
+  answer: string;
+  language: string;
+  reason: string;
+  wrong_answer: string;
+  created_at: Date;
+}
 const supabase = createClient();
 
-// 데이터 가져오는 함수 정의
+// user_answer 데이터 가져오는 함수 정의
 const fetchUserAnswers = async (): Promise<UserAnswer[]> => {
   const { data, error } = await supabase.from("user_answer").select("*");
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+};
+
+// questions 데이터 가져오는 함수 정의
+const fetchQuestions = async (): Promise<Questions[]> => {
+  const { data, error } = await supabase.from("questions").select("*");
 
   if (error) {
     throw new Error(error.message);
@@ -31,25 +53,34 @@ const WrongWordPage = () => {
   // TanStack Query로 데이터 가져오기
   const {
     data: userAnswers,
-    error,
-    isLoading
+    error: userAnswersError,
+    isLoading: userAnswersLoading
   } = useQuery({
     queryKey: ["userAnswers"],
     queryFn: () => fetchUserAnswers()
   });
 
-  console.log("userAnswers", userAnswers);
+  const {
+    data: questions,
+    error: questionsError,
+    isLoading: questionsLoading
+  } = useQuery({
+    queryKey: ["questions"],
+    queryFn: () => fetchQuestions()
+  });
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  console.log("userAnswers", userAnswers);
+  console.log("questions", questions);
+
+  // 로딩 상태
+  if (userAnswersLoading || questionsLoading) return <p>Loading...</p>;
+  // 오류 상태
+  if (userAnswersError) return <p>Error loading user answers: {userAnswersError.message}</p>;
+  if (questionsError) return <p>Error loading questions: {questionsError.message}</p>;
 
   return (
     <div>
       <h1>User Answers</h1>
-      {/* <div>
-        {userAnswers &&
-          userAnswers.map((answer) => <div key={answer.id}>(!answer.is_corrected?){answer.question_id}:null</div>)}
-      </div> */}
       <div>
         {userAnswers &&
           userAnswers.map((answer) => (!answer.is_corrected ? <div key={answer.id}>{answer.question_id}</div> : null))}
