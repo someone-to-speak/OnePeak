1d81ac4b997ffc25f148eb8a2d9e710d4e93316a
byoungyup_kim <qudduq999@daum.net>
2024-10-30 15:37:23 +0900

    feat: 화상 통화 작업

--- src/app/(mobile)/chat/page.tsx
+++ src/app/(mobile)/chat/page.tsx
@@ -9,7 +9,8 @@ type SignalData = {
   candidate?: RTCIceCandidateInit;
 };
 
-const VideoChat: React.FC = () => {
+const VideoChat = () => {
+  console.log("VideoChat");
   const supabase = createClient();
 
   const localVideoRef = useRef<HTMLVideoElement>(null);
@@ -17,22 +18,37 @@ const VideoChat: React.FC = () => {
   const peerConnectionRef = useRef<RTCPeerConnection | null>(null);
 
   const [channel, setChannel] = useState<ReturnType<typeof supabase.channel> | null>(null);
+  const tChannel = useRef<ReturnType<typeof supabase.channel> | null>(null);
 
   useEffect(() => {
-    const signalingChannel = supabase.channel("video-chat");
+    const signalingChannel = supabase.channel("video-chat", {
+      config: {
+        broadcast: { ack: true }
+      }
+    });
     setChannel(signalingChannel);
-
     signalingChannel
-      .on("broadcast", { event: "webrtc-signal" }, () => handleSignalData)
+      .on("broadcast", { event: "ice-candidate" }, (payload: SignalData) => handleSignalData(payload))
+      .on("broadcast", { event: "offer" }, (payload: SignalData) => handleSignalData(payload))
+      .on("broadcast", { event: "answer" }, (payload: SignalData) => handleSignalData(payload))
       .subscribe((status) => {
         if (status === "SUBSCRIBED") {
           console.log("Connected to signaling channel");
+
           initWebRTC();
+        } else if (status === "CHANNEL_ERROR") {
+          console.log("CHANNEL_ERROR");
+        } else if (status === "CLOSED") {
+          console.log("CLOSED");
+        } else if (status === "TIMED_OUT") {
+          console.log("TIMED_OUT");
         }
       });
 
+    // tChannel.current = signalingChannel;
+
     return () => {
-      signalingChannel.unsubscribe();
+      channel?.unsubscribe();
       peerConnectionRef.current?.close();
     };
   }, []);
@@ -43,15 +59,35 @@ const VideoChat: React.FC = () => {
     peerConnectionRef.current = peerConnection;
 
     peerConnection.onicecandidate = (event) => {
+      // console.log("onicecandidate event: :", event);
       if (event.candidate) {
-        channel?.send({ type: "broadcast", event: "ice-candidate", candidate: event.candidate });
+        // console.log("send candidate");
+        channel?.send({
+          type: "broadcast",
+          event: "ice-candidate",
+          candidate: event.candidate
+        });
       }
     };
 
     peerConnection.ontrack = (event) => {
+      // console.log("ontrack");
       if (remoteVideoRef.current) {
+        // console.log("peerConnection: ", peerConnection);
         remoteVideoRef.current.srcObject = event.streams[0];
       }
+    };
+
+    peerConnection.onconnectionstatechange = () => {
+      console.log("Connection State:", peerConnection.connectionState);
+    };
+
+    peerConnection.oniceconnectionstatechange = () => {
+      console.log("ICE Connection State:", peerConnection.iceConnectionState);
+    };
+
+    peerConnection.onicegatheringstatechange = () => {
+      console.log("ICE Gathering State:", peerConnection.iceGatheringState);
     };
 
     const localStream = await navigator.mediaDevices.getUserMedia({
@@ -67,38 +103,91 @@ const VideoChat: React.FC = () => {
   };
 
   const handleSignalData = async (payload: SignalData) => {
-    if (!peerConnectionRef.current) return;
-
-    const { event, sdp, candidate } = payload;
-
-    if (event === "offer" && sdp) {
-      await peerConnectionRef.current.setRemoteDescription(new RTCSessionDescription(sdp));
-      const answer = await peerConnectionRef.current.createAnswer();
-      await peerConnectionRef.current.setLocalDescription(answer);
-      channel?.send({ type: "broadcast", event: "answer", sdp: answer });
-    } else if (event === "answer" && sdp) {
-      await peerConnectionRef.current.setRemoteDescription(new RTCSessionDescription(sdp));
-    } else if (event === "ice-candidate" && candidate) {
-      await peerConnectionRef.current.addIceCandidate(new RTCIceCandidate(candidate));
+    try {
+      if (!peerConnectionRef.current) return;
+      console.log("payload: ", payload);
+      const { event, sdp, candidate } = payload;
+      console.log("sdp: ", sdp);
+      if (event === "offer" && sdp) {
+        await peerConnectionRef.current.setRemoteDescription(new RTCSessionDescription(sdp));
+        const answer = await peerConnectionRef.current.createAnswer();
+        await peerConnectionRef.current.setLocalDescription(answer);
+        // console.log("channel: ", channel);
+        const serverResponse = channel?.send({
+          type: "broadcast",
+          event: "answer",
+          sdp: answer
+        });
+        console.log("serverResponse: ", serverResponse);
+      } else if (event === "answer" && sdp) {
+        console.log("answer");
+        await peerConnectionRef.current.setRemoteDescription(new RTCSessionDescription(sdp));
+      } else if (event === "ice-candidate" && candidate) {
+        console.log("candidate");
+        await peerConnectionRef.current.addIceCandidate(new RTCIceCandidate(candidate));
+      }
+      console.log("end");
+    } catch (error) {
+      console.error("Error handling signal data:", error);
     }
   };
 
+  // const handleOfferSignal = async (payload: SignalData) => {
+  //   if (!peerConnectionRef.current) return;
+  //   console.log("payload: ", payload);
+  //   const { sdp } = payload;
+  //   if (sdp?.sdp) {
+  //     await peerConnectionRef.current.setRemoteDescription(new RTCSessionDescription(sdp));
+  //     const answer = await peerConnectionRef.current.createAnswer();
+  //     console.log("answer: ", answer);
+  //     await peerConnectionRef.current.setLocalDescription(answer);
+  //     const serverResponse = channel?.send({ type: "broadcast", event: "answer", payload: { sdp: answer } });
+  //     console.log("serverResponse: ", serverResponse);
+  //   }
+  // };
+
+  // const handleAnswerSignal = async (payload: SignalData) => {
+  //   if (!peerConnectionRef.current) return;
+
+  //   const { sdp } = payload;
+  //   if (sdp) {
+  //     await peerConnectionRef.current.setRemoteDescription(new RTCSessionDescription(sdp));
+  //   }
+  // };
+
+  // const handleCandidateSignal = async (payload: SignalData) => {
+  //   if (!peerConnectionRef.current) return;
+
+  //   const { candidate } = payload;
+  //   if (candidate) await peerConnectionRef.current.addIceCandidate(new RTCIceCandidate(candidate));
+  // };
+
   const createOffer = async () => {
-    if (!peerConnectionRef.current) return;
+    if (!peerConnectionRef.current) {
+      console.log("not found peerConnectionRef");
+      return;
+    }
 
     const offer = await peerConnectionRef.current.createOffer();
     await peerConnectionRef.current.setLocalDescription(offer);
-    channel?.send({ type: "broadcast", event: "offer", sdp: offer });
+    const serverResponse = channel?.send({
+      type: "broadcast",
+      event: "offer",
+      sdp: offer
+    });
+    console.log("serverResponse: ", serverResponse);
+    // console.log("re channel: ", channel);
   };
 
   return (
     <div>
       <h1>1:1 화상 채팅</h1>
-      <div>
-        <video className="w-[300px] aspect-video" ref={localVideoRef} autoPlay playsInline muted />
-        <video className="w-[300px] aspect-video" ref={remoteVideoRef} autoPlay playsInline />
-      </div>
       <button onClick={createOffer}>Start Call</button>
+
+      <div className="flex flex-col h-auto">
+        <video ref={remoteVideoRef} autoPlay />
+        <video ref={localVideoRef} autoPlay />
+      </div>
     </div>
   );
 };
