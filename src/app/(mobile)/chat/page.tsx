f0b0af69bee5293b7ffd3b40ce9ced3beb18818e
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-10-30 19:40:37 +0900

    edit: 오픈AI API Key 이름 변경

--- src/app/(mobile)/chat/page.tsx
+++ src/app/(mobile)/chat/page.tsx
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 import React from "react";
 
 const openai = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY
 });
 
 const Page = async () => {
