d0dcf55400d1b426881872019416622135117740
dajung <kwonkkan@naver.com>
2024-10-31 00:16:31 +0900

    feat: user_info, review 테이블과 연결

--- src/components/chatBot/aiTutorHome/TodayLearn.tsx
+++ src/components/chatBot/aiTutorHome/TodayLearn.tsx
@@ -4,12 +4,22 @@ import { createClient } from "@/utils/supabase/client";
 import { useEffect, useState } from "react";
 import { Tables } from "../../../../database.types";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 type SituationType = Tables<"situation">;
 
 const TodayLearn = () => {
   const supabase = createClient();
   const [situations, setSituations] = useState<SituationType[]>([]);
+  const router = useRouter();
+
+  // 유저 정보 조회
+  const getUserInfo = async () => {
+    const {
+      data: { user }
+    } = await supabase.auth.getUser();
+    return user;
+  };
 
   // situation 조회
   const getSituations = async () => {
@@ -29,12 +39,43 @@ const TodayLearn = () => {
     }
   };
 
+  // review 테이블에 유저가 선택한 학습 추가
+  const addReview = async (userId: string, situation: string, level: number) => {
+    const { data, error } = await supabase
+      .from("review")
+      .insert([
+        {
+          user_id: userId, // 외래키로 연결된 유저의 ID
+          situation,
+          level
+        }
+      ])
+      .select();
+
+    if (error) {
+      console.log("review 테이블 추가 오류: ", error);
+    }
+  };
+
+  // 버튼 핸들러
+  const handleLearnSelect = async (e: { preventDefault: () => void }, situation: string, level: number) => {
+    e.preventDefault();
+
+    const user = await getUserInfo(); // 유저 정보 가져오기
+    if (user) {
+      await addReview(user.id, situation, level);
+
+      // 데이터 추가 후 이동
+      router.push(`/chatbot?situation=${situation}&level=${level}`);
+    }
+  };
+
   useEffect(() => {
+    getUserInfo();
     getSituations();
   }, []);
 
   // TODO: 기능 구현 후 캐러셀 적용
-
   return (
     <div className="h-64">
       <h1 className="text-3xl font-bold">오늘의 학습</h1>
@@ -44,7 +85,8 @@ const TodayLearn = () => {
           return (
             <Link
               key={situation.id}
-              href={{ pathname: "/chatbot", query: { situation: situation.situation, level: situation.level } }}
+              href={`/chatbot?situation=${situation.situation}&level=${situation.level}`}
+              onClick={(e) => handleLearnSelect(e, situation.situation, situation.level)}
             >
               <div className="w-60 h-60 border border-spacing-2">
                 <p>{situation.situation}</p>
