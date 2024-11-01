c8ca21a75b632174f4717f16c845545c512c3c0a
byoungyup_kim <qudduq999@daum.net>
2024-11-01 17:41:49 +0900

    fix: 에러 처리

--- src/hooks/useMatching.ts
+++ src/hooks/useMatching.ts
@@ -22,8 +22,8 @@ export const useMatching = () => {
     const roomId = await initiateMatching(userInfo.id, userInfo.my_language, userInfo.learn_language);
 
     if (roomId) {
-      router.push(`/chat?room=${roomId}`);
       setIsMatching(false);
+      router.push(`/chat?room=${roomId}`);
     } else {
       const matchingChannel = supabase.channel("matches");
       matchingChannelRef.current = matchingChannel;
@@ -33,11 +33,15 @@ export const useMatching = () => {
           console.log("UPDATE");
           const { new: updatedMatchQueue } = payload;
           if (updatedMatchQueue.user_id === userInfo.id) {
-            router.push(`/chat?room=${updatedMatchQueue.room_id}`);
             setIsMatching(false);
+            router.push(`/chat?room=${updatedMatchQueue.room_id}`);
           }
         })
-        .subscribe();
+        .subscribe((status) => {
+          if (status === "SUBSCRIBED") {
+            console.log("SUBSCRIBED");
+          }
+        });
     }
   };
 
