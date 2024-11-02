1a781d25e2b7064d2c91607cf2485e991fe8f03a
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-11-02 15:07:14 +0900

    style: 바텀시트모달 버튼보이게 수정

--- src/components/BottomSheetModal.tsx
+++ src/components/BottomSheetModal.tsx
@@ -12,19 +12,27 @@ const BottomSheetModal: React.FC<BottomSheetModalProps> = ({ onClose }) => {
   };
 
   return (
-    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-end z-50">
+    <div className="fixed inset-12 bg-gray-500 bg-opacity-75 flex justify-center items-end z-20">
       <div className="bg-white w-full max-w-md p-6 rounded-t-lg">
         <button onClick={onClose} className="text-gray-500 float-right">
           닫기
         </button>
         <h2 className="text-xl font-bold mb-4">로그인이 필요합니다</h2>
         <p className="mb-4">서비스를 이용하려면 로그인이 필요합니다.</p>
-        <button onClick={handleSignInWithGoogle} className="w-full bg-blue-500 text-white py-2 rounded">
-          구글
-        </button>
-        <button type="button">카카오</button>
-        <button type="button">네이버</button>
-        <button type="button">애플</button>
+        <div className="flex gap-4">
+          <button onClick={handleSignInWithGoogle} className=" bg-blue-500 text-white py-2 rounded">
+            구글
+          </button>
+          <button type="button" className=" bg-blue-500 text-white py-2 rounded">
+            카카오
+          </button>
+          <button type="button" className=" bg-blue-500 text-white py-2 rounded">
+            네이버
+          </button>
+          <button type="button" className=" bg-blue-500 text-white py-2 rounded">
+            애플
+          </button>
+        </div>
       </div>
     </div>
   );
