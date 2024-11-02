28990081107640aa3de0f548a0cc01eeba169c2c
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-11-02 14:57:47 +0900

    label: 모달닫기 위한 콜백함수 타입정의

--- src/components/BottomSheetModal.tsx
+++ src/components/BottomSheetModal.tsx
@@ -1,7 +1,12 @@
 import { signInWithProvider } from "@/app/services/supabaseAuth";
 import React from "react";
 
-const BottomSheetModal = () => {
+// 모달을 닫기 위한 콜백 함수 타입 정의
+interface BottomSheetModalProps {
+  onClose: () => void;
+}
+
+const BottomSheetModal: React.FC<BottomSheetModalProps> = ({ onClose }) => {
   const handleSignInWithGoogle = async () => {
     await signInWithProvider("google");
   };
@@ -9,9 +14,9 @@ const BottomSheetModal = () => {
   return (
     <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-end z-50">
       <div className="bg-white w-full max-w-md p-6 rounded-t-lg">
-        {/* <button onClick={onClose} className="text-gray-500 float-right">
+        <button onClick={onClose} className="text-gray-500 float-right">
           닫기
-        </button> */}
+        </button>
         <h2 className="text-xl font-bold mb-4">로그인이 필요합니다</h2>
         <p className="mb-4">서비스를 이용하려면 로그인이 필요합니다.</p>
         <button onClick={handleSignInWithGoogle} className="w-full bg-blue-500 text-white py-2 rounded">
