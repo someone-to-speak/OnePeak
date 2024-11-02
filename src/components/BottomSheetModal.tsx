2cd3b006e2e01c2c22d15cdaca9f1be9427799dd
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-11-02 14:44:29 +0900

    add: 로그인 바텀시트 모달 생성

---
+++ src/components/BottomSheetModal.tsx
@@ -0,0 +1,28 @@
+import { signInWithProvider } from "@/app/services/supabaseAuth";
+import React from "react";
+
+const BottomSheetModal = () => {
+  const handleSignInWithGoogle = async () => {
+    await signInWithProvider("google");
+  };
+
+  return (
+    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-end z-50">
+      <div className="bg-white w-full max-w-md p-6 rounded-t-lg">
+        {/* <button onClick={onClose} className="text-gray-500 float-right">
+          닫기
+        </button> */}
+        <h2 className="text-xl font-bold mb-4">로그인이 필요합니다</h2>
+        <p className="mb-4">서비스를 이용하려면 로그인이 필요합니다.</p>
+        <button onClick={handleSignInWithGoogle} className="w-full bg-blue-500 text-white py-2 rounded">
+          구글
+        </button>
+        <button type="button">카카오</button>
+        <button type="button">네이버</button>
+        <button type="button">애플</button>
+      </div>
+    </div>
+  );
+};
+
+export default BottomSheetModal;
