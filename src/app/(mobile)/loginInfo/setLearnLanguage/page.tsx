57c25a6d4c19ed8e0f35fa4197c96d7caf077f54
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-11-03 04:30:41 +0900

    feat: 지원언어 데이터로 목록 나타내기

--- src/app/(mobile)/loginInfo/setLearnLanguage/page.tsx
+++ src/app/(mobile)/loginInfo/setLearnLanguage/page.tsx
@@ -3,13 +3,30 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { createClient } from "@/utils/supabase/client";
+import { useQuery } from "@tanstack/react-query";
+import { fetchLanguageName } from "@/api/firstSetting/fetchLanguageName";
 
 const learnLanguages = ["영어", "일본어", "스페인어", "프랑스어"];
 
 export default function SetLearnLanguage() {
-  const [selectedLearnLanguage, setSelectedLearnLanguage] = useState<string | null>(null);
+  const [selectedLearnLanguage, setSelectedLearnLanguage] = useState<string>("");
   const router = useRouter();
   const supabase = createClient();
+
+  // Tanstack Query - 지원언어 데이터 가져오기
+  const {
+    data: languages,
+    error: languagesError,
+    isLoading: languagesLoading
+  } = useQuery({
+    queryKey: ["language"],
+    queryFn: () => fetchLanguageName()
+  });
+
+  // 로딩 상태
+  if (languagesLoading) return <p>Loading...</p>;
+  // 오류 상태
+  if (languagesError) return <p>Error loading user answers: {languagesError.message}</p>;
 
   const handleContinue = async () => {
     const { data } = await supabase.auth.getSession();
@@ -22,7 +39,7 @@ export default function SetLearnLanguage() {
       const { error } = await supabase
         .from("user_info")
         .update({ learn_language: selectedLearnLanguage })
-        .eq("user_id", userId);
+        .eq("id", userId);
 
       if (!error) {
         router.push("/"); // 홈화면으로 이동
@@ -35,13 +52,15 @@ export default function SetLearnLanguage() {
       <h1>너의 학습언어 알려줘</h1>
       <p>본인이 배우고 싶은 언어를 설정해주시면 됩니다.</p>
       <div className="flex flex-col justify-center items-center">
-        {learnLanguages.map((language) => (
+        {languages?.map((language, index) => (
           <button
-            key={language}
-            onClick={() => setSelectedLearnLanguage(language)}
-            className={`w-[50%] p-2 m-2 rounded ${selectedLearnLanguage === language ? "bg-green-500" : "bg-gray-300"}`}
+            key={index}
+            onClick={() => setSelectedLearnLanguage(language.language_name)}
+            className={`w-[50%] p-2 m-2 rounded ${
+              selectedLearnLanguage === language.language_name ? "bg-green-500" : "bg-gray-300"
+            }`}
           >
-            {language}
+            {language.language_name}
           </button>
         ))}
       </div>
--- src/app/(mobile)/loginInfo/setMyLanguage/page.tsx
+++ src/app/(mobile)/loginInfo/setMyLanguage/page.tsx
@@ -3,13 +3,28 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { createClient } from "@/utils/supabase/client";
-
-const languages = ["한국어", "일본어", "스페인어", "영어"];
+import { useQuery } from "@tanstack/react-query";
+import { fetchLanguageName } from "@/api/firstSetting/fetchLanguageName";
 
 export default function SetMyLanguage() {
-  const [selectedMyLanguage, setSelectedMyLanguage] = useState<string | null>(null);
+  const [selectedMyLanguage, setSelectedMyLanguage] = useState<string>("");
   const router = useRouter();
   const supabase = createClient();
+
+  // Tanstack Query - 지원언어 데이터 가져오기
+  const {
+    data: languages,
+    error: languagesError,
+    isLoading: languagesLoading
+  } = useQuery({
+    queryKey: ["language"],
+    queryFn: () => fetchLanguageName()
+  });
+
+  // 로딩 상태
+  if (languagesLoading) return <p>Loading...</p>;
+  // 오류 상태
+  if (languagesError) return <p>Error loading user answers: {languagesError.message}</p>;
 
   const handleContinue = async () => {
     const { data } = await supabase.auth.getSession();
@@ -19,10 +34,7 @@ export default function SetMyLanguage() {
     console.log("mylanguage to update:", selectedMyLanguage); // 업데이트할 닉네임 확인
 
     if (userId && selectedMyLanguage) {
-      const { error } = await supabase
-        .from("user_info")
-        .update({ my_language: selectedMyLanguage })
-        .eq("user_id", userId);
+      const { error } = await supabase.from("user_info").update({ my_language: selectedMyLanguage }).eq("id", userId);
 
       if (!error) {
         router.push("/loginInfo/setLearnLanguage");
@@ -35,13 +47,15 @@ export default function SetMyLanguage() {
       <h1>너의 모국어 알려줘</h1>
       <p>본인의 모국어를 설정해주시면 됩니다.</p>
       <div className="flex flex-col justify-center items-center">
-        {languages.map((language) => (
+        {languages?.map((language, index) => (
           <button
-            key={language}
-            onClick={() => setSelectedMyLanguage(language)}
-            className={`w-[50%] p-2 m-2 rounded ${selectedMyLanguage === language ? "bg-green-500" : "bg-gray-300"}`}
+            key={index}
+            onClick={() => setSelectedMyLanguage(language.language_name)}
+            className={`w-[50%] p-2 m-2 rounded ${
+              selectedMyLanguage === language.language_name ? "bg-green-500" : "bg-gray-300"
+            }`}
           >
-            {language}
+            {language.language_name}
           </button>
         ))}
       </div>
