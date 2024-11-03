597d873f8b77f1029d147979438c0ff03d018191
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-11-03 20:06:19 +0900

    refactor: 지원 중인 언어로 필터

--- src/api/firstSetting/fetchLanguageName.ts
+++ src/api/firstSetting/fetchLanguageName.ts
@@ -3,10 +3,9 @@ import { createClient } from "@/utils/supabase/client";
 type LanguageName = { language_name: string };
 const supabase = createClient();
 
-// 'language'테이블에서 'language_name'만 가져오는 함수
+// 'language'테이블에서 status가 true인 'language_name'만 가져오는 함수(=지원 중인 언어)
 export const fetchLanguageName = async (): Promise<LanguageName[]> => {
-  const { data, error } = await supabase.from("language").select("language_name");
-  console.log("지원언어", data);
+  const { data, error } = await supabase.from("language").select("language_name").eq("status", true);
   if (error) {
     throw new Error(error.message);
   }
