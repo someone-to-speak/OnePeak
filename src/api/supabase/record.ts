ddbaeeea3b2c34165034c519661dc4321c59eaee
byoungyup_kim <qudduq999@daum.net>
2024-11-04 19:23:01 +0900

    fix: 에러 처리

--- src/api/supabase/record.ts
+++ src/api/supabase/record.ts
@@ -3,20 +3,20 @@ import { createClient } from "@/utils/supabase/client";
 const supabase = createClient();
 
 export const uploadRecording = async (fileBlob: Blob, fileName: string): Promise<string> => {
-  await supabase.storage.from("recordings").upload(fileName, fileBlob, {
+  const { data } = await supabase.storage.from("recordings").upload(fileName, fileBlob, {
     contentType: "audio/webm"
   });
 
   const {
     data: { publicUrl }
-  } = supabase.storage.from("recordings").getPublicUrl(fileName);
+  } = supabase.storage.from("recordings").getPublicUrl(data?.path as string);
 
   return publicUrl;
 };
 
 export const downloadFileData = async (url: string) => {
-  const strippedUrl = url.replace("https://", "");
-  const { data: fileData, error } = await supabase.storage.from("recordings").download(strippedUrl);
+  const { data: fileData, error } = await supabase.storage.from("recordings").download(url);
+  console.log("fileData:  ", fileData);
   console.log("error: ", error);
   const audioUrl = URL.createObjectURL(fileData as Blob);
   return audioUrl as string;
