496f1a987d7c43ece0a6d4ec1c4008a0078d560e
byoungyup_kim <qudduq999@daum.net>
2024-11-05 16:01:57 +0900

    fix: 타입 에러 처리

--- src/api/supabase/chat.ts
+++ src/api/supabase/chat.ts
@@ -1,4 +1,9 @@
-import { MessageWithUserInfo, UserInfoWithLanguage } from "@/types/chatType/chatType";
+import {
+  ConversationWithParticipants,
+  Message,
+  MessageWithUserInfo,
+  UserInfoWithLanguage
+} from "@/types/chatType/chatType";
 import { UserInfo } from "@/types/userType/userType";
 import { createClient } from "@/utils/supabase/client";
 import { RealtimeChannel } from "@supabase/supabase-js";
@@ -55,28 +60,26 @@ export const fetchConversationList = async (userId: string) => {
     .select(
       `
       *,
-      last_message: last_message_id(*)
+      last_message_id(*)
   `
     )
     .in("id", conversationIds as string[])
     .order("updated_at", { ascending: false });
 
-  // const formattedConversationList = conversationList?.map((list) => ({
-  //   ...list,
-  //   last_message: list.last_message && list.last_message[0] ? (list.last_message[0] as Message) : null
-  // }));
-  console.log("conversationList: ", conversationList);
+  const formattedConversationList = conversationList?.map((list) => ({
+    ...list,
+    last_message_id: (list?.last_message_id || {}) as Message
+  }));
+
   // console.log("formattedConversationList: ", formattedConversationList);
   const conversationListWithParticipants = await Promise.all(
-    conversationList?.map(async (conversation) => {
+    formattedConversationList?.map(async (conversation) => {
       const { data: participants } = await supabase
         .from("participants")
         .select("*, user_info: user_id(*, my_language(*), learn_language(*))")
         .eq("conversation_id", conversation.id)
         .neq("user_id", userId)
         .single();
-
-      console.log("participants: ", participants);
 
       return {
         ...conversation,
@@ -87,8 +90,8 @@ export const fetchConversationList = async (userId: string) => {
       };
     }) || []
   );
-  console.log("conversationListWithParticipants: ", conversationListWithParticipants);
-  return conversationListWithParticipants;
+
+  return conversationListWithParticipants as ConversationWithParticipants[];
 };
 
 // 메시지 불러오기
