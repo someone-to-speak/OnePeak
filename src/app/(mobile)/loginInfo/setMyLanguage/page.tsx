89f7e53fa05f1bd79fe9b663a6f34006b5106e32
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-11-05 17:28:31 +0900

    style: 내언어 초기설정 페이지

--- src/app/(mobile)/loginInfo/setMyLanguage/page.tsx
+++ src/app/(mobile)/loginInfo/setMyLanguage/page.tsx
@@ -5,6 +5,8 @@ import { useRouter } from "next/navigation";
 import { createClient } from "@/utils/supabase/client";
 import { useQuery } from "@tanstack/react-query";
 import { fetchLanguageName } from "@/api/firstSetting/fetchLanguageName";
+import caretleft from "@/../public/images/CaretLeft.svg";
+import Image from "next/image";
 
 export default function SetMyLanguage() {
   const [selectedMyLanguage, setSelectedMyLanguage] = useState<string>("");
@@ -42,35 +44,55 @@ export default function SetMyLanguage() {
   };
 
   return (
-    <div>
-      <button
-        onClick={() => router.back()} // 뒤로 가기 함수 호출
-        className="mb-4 font-bold"
-      >
-        ←
-      </button>
-      <div className="flex flex-col items-center">
-        <h1>모국어를 선택해 주세요</h1>
-        <p>모국어를 설정해 주세요</p>
+    <>
+      <div className="px-4 py-3 mb-[10px] h-12 flex gap-[6px]">
+        <button
+          onClick={() => router.back()} // 뒤로 가기 함수 호출
+          className="mb-4 font-bold"
+        >
+          <Image src={caretleft} alt={"CaretLeft"} />
+        </button>
+        <></>
       </div>
-      <div className="flex flex-col justify-center items-center">
-        {supportingLanguages?.map((language, index) => (
-          <button
-            key={index}
-            onClick={() => setSelectedMyLanguage(language)}
-            className={`w-[50%] p-2 m-2 rounded ${selectedMyLanguage === language ? "bg-green-500" : "bg-gray-300"}`}
-          >
-            {language}
-          </button>
-        ))}
+      <div className="px-4">
+        <div className="mb-6 flex flex-col items-center gap-1">
+          <h1 className="text-[var(--Primary-50,#020401)] text-center font-suit text-[28px] font-bold leading-[42px] tracking-[-0.56px]">
+            모국어를 선택해 주세요
+          </h1>
+          <p className="text-[var(--Gray-500,#8C8C8C)] text-center font-pretendard text-[14px] font-medium leading-[21px] tracking-[-0.28px]">
+            모국어를 설정해 주세요
+          </p>
+        </div>
+        <div className="w-full flex flex-col items-start gap-2 shrink-0">
+          {supportingLanguages?.map((language, index) => (
+            <button
+              key={index}
+              onClick={() => setSelectedMyLanguage(language)}
+              className={`"flex h-[64px] py-[10px] px-[20px] text-left gap-[10px] shrink-0 self-stretch rounded-[10px] "
+        ${
+          selectedMyLanguage === language
+            ? "border border-[var(--Secondary-500, #FFA500)] bg-[var(--Secondary-900, #FFEDCC)]"
+            : "border border-[var(--Gray-800, #D9D9D9)] bg-[var(--White, #FDFDFD)]"
+        }`}
+            >
+              <p>{language}</p>
+            </button>
+          ))}
+        </div>
       </div>
-      <button
-        onClick={handleContinue}
-        disabled={!selectedMyLanguage}
-        className={`w-full mt-4 p-2 rounded ${selectedMyLanguage ? "bg-green-500" : "bg-gray-300"}`}
-      >
-        계속
-      </button>
-    </div>
+      <div className="relative z-0 bg-[var(--White, #FDFDFD)] py-[10px] px-4">
+        <button
+          onClick={handleContinue}
+          disabled={!selectedMyLanguage}
+          className={`flex w-full h-[54px] p-[10px] justify-center items-center gap-[10px] flex-shrink-0 rounded-[10px] mb-[10px] ${
+            selectedMyLanguage
+              ? "opacity-100 bg-[var(--Primary-500,#7BD232)]"
+              : "opacity-40 bg-[var(--Primary-500,#7BD232)]"
+          }`}
+        >
+          계속
+        </button>
+      </div>
+    </>
   );
 }
