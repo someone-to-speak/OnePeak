81a50e353a9a078f6587f1281b07ae89853f1c39
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-11-05 15:25:25 +0900

    style: 닉네임 초기설정 페이지

--- src/app/(mobile)/loginInfo/setNickname/page.tsx
+++ src/app/(mobile)/loginInfo/setNickname/page.tsx
@@ -26,13 +26,18 @@ export default function SetNickname() {
   };
 
   return (
-    <div>
-      <div className="flex flex-col items-center">
-        <h1>닉네임을 입력해 주세요</h1>
-        <p>원픽에서 사용할 닉네임을 설정해 주세요</p>
+    <div className="p-4 gap-[10px]">
+      <div className="h-12"></div>
+      <div className="flex flex-col items-center gap-1">
+        <h1 className="text-[var(--Primary-50,#020401)] text-center font-suit text-[28px] font-bold leading-[42px] tracking-[-0.56px]">
+          닉네임을 입력해 주세요
+        </h1>
+        <p className="text-[var(--Gray-500,#8C8C8C)] text-center font-pretendard text-[14px] font-medium leading-[21px] tracking-[-0.28px]">
+          원픽에서 사용할 닉네임을 설정해 주세요
+        </p>
       </div>
-      <div className="flex flex-col">
-        <p className="flex justify-end">
+      <div className="flex flex-col gap-[6px] mb-[447px]">
+        <p className="text-[var(--Gray-500,#8C8C8C)] text-right font-pretendard text-[12px] font-medium leading-[18px] tracking-[-0.24px]">
           {nickname.length}/{maxTexts}
         </p>
         <input
@@ -40,14 +45,16 @@ export default function SetNickname() {
           value={nickname}
           onChange={(e) => setNickname(e.target.value)}
           placeholder="닉네임을 입력해 주세요"
-          className="border rounded p-2"
+          className="flex h-[50px] px-[20px] py-[10px] items-center gap-[10px] self-stretch rounded-[12px] border border-[var(--Gray-600,#A6A6A6)] bg-[var(--White,#FDFDFD]"
           maxLength={maxTexts} // 최대 글자 수 제한
         />
       </div>
       <button
         onClick={handleContinue}
         disabled={!nickname}
-        className={`w-full mt-4 p-2 rounded ${nickname ? "bg-green-500" : "bg-gray-300"}`}
+        className={`flex w-full h-[54px] p-[10px] justify-center items-center gap-[10px] flex-shrink-0 rounded-[10px] mb-[10px] ${
+          nickname ? "opacity-100 bg-[var(--Primary-500,#7BD232)]" : "opacity-40 bg-[var(--Primary-500,#7BD232)]"
+        }`}
       >
         계속
       </button>
