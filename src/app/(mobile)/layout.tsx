42b446e6df08a0c19bd5b12db12738a3cd2c86f1
byoungyup_kim <qudduq999@daum.net>
2024-11-06 02:37:03 +0900

    style: 화상 통화 스타일 작업

--- src/app/(mobile)/layout.tsx
+++ src/app/(mobile)/layout.tsx
@@ -18,13 +18,13 @@ export default function MobileLayout({
 
   // pathname이 null이 아니면 특정 경로에서 헤더와 푸터 숨기기
   // const hideHeaderFooter = pathname !== null && ["/loginInfo/*"].includes(pathname);
-  const hideHeaderFooter = pathname !== null && pathname.startsWith("/loginInfo");
+  const hideHeaderFooter = pathname === "/loginInfo" || "/leeson/room";
 
   return (
     <div>
-      <div className="w-full min-w-[320px] max-w-[600px] mx-auto my-0  min-h-full">
+      <div className="w-full flex flex-col min-w-[320px] max-w-[600px] mx-auto my-0  min-h-full">
         {!hideHeaderFooter && <Header />}
-        {children}
+        <main>{children}</main>
         {!hideHeaderFooter && <Navibar />}
       </div>
     </div>
