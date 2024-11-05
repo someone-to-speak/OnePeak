74bfe4685f0a929a56a1294efe5295b47e673279
Seoyun Kim <seoyunkim@Seoyunui-MacBookPro.local>
2024-11-05 21:56:17 +0900

    add: finish style/noti

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -10,14 +10,12 @@ const BackButton: React.FC<BackButtonProps> = ({ title }) => {
   const router = useRouter();
 
   return (
-    <div className="w-full h-12 justify-start items-center gap-1.5 inline-flex">
-      <button onClick={() => router.back()} className="flex flex-row items-center">
-        <Image
-          src={caretleft}
-          alt={"CaretLeft"}
-          className="grow shrink basis-0 text-[#0c0c0c] text-lg font-bold font-['SUIT'] leading-[27px]"
-        />
-        {title}
+    <div className="w-full h-12 justify-start inline-flex">
+      <button onClick={() => router.back()} className="text-[#0c0c0c] text-lg font-bold font-['SUIT'] leading-[27px]">
+        <div className="flex flex-row items-center gap-1.5">
+          <Image src={caretleft} alt={"CaretLeft"} className="w-6 h-6 left-0 top-0" />
+          {title}
+        </div>
       </button>
     </div>
   );
