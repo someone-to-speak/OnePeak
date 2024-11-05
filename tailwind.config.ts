07cc43b4226dc30f1792f30e214c53e49d6d7354
mingjeongg <alswjd6141@naver.com>
2024-11-05 22:54:46 +0900

    공통컴포넌트  만드는 중

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from "tailwindcss";
 import safeArea from "tailwindcss-safe-area";
-import tailwindAnimate from "tailwindcss-animate";
 const config: Config = {
   darkMode: ["class"],
   content: [
@@ -22,85 +21,77 @@ const config: Config = {
           foreground: "hsl(var(--popover-foreground))"
         },
         primary: {
-          50: "var(--primary-50)",
-          100: "var(--primary-100)",
-          200: "var(--primary-200)",
-          300: "var(--primary-300)",
-          400: "var(--primary-400)",
-          500: "var(--primary-500)",
-          600: "var(--primary-600)",
-          700: "var(--primary-700)",
-          800: "var(--primary-800)",
-          900: "var(--primary-900)"
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))"
         },
         secondary: {
-          50: "var(--secondary-50)",
-          100: "var(--secondary-100)",
-          200: "var(--secondary-200)",
-          300: "var(--secondary-300)",
-          400: "var(--secondary-400)",
-          500: "var(--secondary-500)",
-          600: "var(--secondary-600)",
-          700: "var(--secondary-700)",
-          800: "var(--secondary-800)",
-          900: "var(--secondary-900)"
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))"
         },
-        gray: {
-          50: "var(--gray-50)",
-          100: "var(--gray-100)",
-          200: "var(--gray-200)",
-          300: "var(--gray-300)",
-          400: "var(--gray-400)",
-          500: "var(--gray-500)",
-          600: "var(--gray-600)",
-          700: "var(--gray-700)",
-          800: "var(--gray-800)",
-          900: "var(--gray-900)"
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))"
         },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))"
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+          gray: {
+            50: "var(--gray-50)",
+            100: "var(--gray-100)",
+            200: "var(--gray-200)",
+            300: "var(--gray-300)",
+            400: "var(--gray-400)",
+            500: "var(--gray-500)",
+            600: "var(--gray-600)",
+            700: "var(--gray-700)",
+            800: "var(--gray-800)",
+            900: "var(--gray-900)"
+          },
+          destructive: {
+            DEFAULT: "hsl(var(--destructive))",
+            foreground: "hsl(var(--destructive-foreground))"
+          },
+          border: "hsl(var(--border))",
+          input: "hsl(var(--input))",
+          ring: "hsl(var(--ring))",
+          chart: {
+            "1": "hsl(var(--chart-1))",
+            "2": "hsl(var(--chart-2))",
+            "3": "hsl(var(--chart-3))",
+            "4": "hsl(var(--chart-4))",
+            "5": "hsl(var(--chart-5))"
+          }
         },
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        chart: {
-          "1": "hsl(var(--chart-1))",
-          "2": "hsl(var(--chart-2))",
-          "3": "hsl(var(--chart-3))",
-          "4": "hsl(var(--chart-4))",
-          "5": "hsl(var(--chart-5))"
+        borderRadius: {
+          lg: "var(--border-radius-37)",
+          md: "var(--border-radius-26)",
+          sm: "var(--border-radius-20)"
+        },
+        fontSize: {
+          sm: "var(--font-sm)",
+          base: "var(--font-base)",
+          lg: "var(--font-lg)",
+          xl: "var(--font-xl)",
+          "2xl": "var(--font-2xl)",
+          "3xl": "var(--font-3xl)",
+          "4xl": "var(--font-4xl)",
+          "5xl": "var(--font-5xl)",
+          "6xl": "var(--font-6xl)",
+          "7xl": "var(--font-7xl)"
+        },
+        fontFamily: {
+          suit: "var(--font-family-suit)",
+          pretendard: "var(--font-family-pretendard)"
+        },
+        boxShadow: {
+          category: "var(--shadow-category)",
+          review: "var(--shadow-review)",
+          background: "var(--shadow-background)",
+          challengeTab: "var(--shadow-challenge-tab)"
         }
-      },
-      borderRadius: {
-        lg: "var(--border-radius-37)",
-        md: "var(--border-radius-26)",
-        sm: "var(--border-radius-20)"
-      },
-      fontSize: {
-        sm: "var(--font-sm)",
-        base: "var(--font-base)",
-        lg: "var(--font-lg)",
-        xl: "var(--font-xl)",
-        "2xl": "var(--font-2xl)",
-        "3xl": "var(--font-3xl)",
-        "4xl": "var(--font-4xl)",
-        "5xl": "var(--font-5xl)",
-        "6xl": "var(--font-6xl)",
-        "7xl": "var(--font-7xl)"
-      },
-      fontFamily: {
-        suit: "var(--font-family-suit)",
-        pretendard: "var(--font-family-pretendard)"
-      },
-      boxShadow: {
-        category: "var(--shadow-category)",
-        review: "var(--shadow-review)",
-        background: "var(--shadow-background)",
-        challengeTab: "var(--shadow-challenge-tab)"
       }
-    }
-  },
-  plugins: [safeArea, tailwindAnimate]
+    },
+    plugins: [safeArea, require("tailwindcss-animate")]
+  }
 };
 export default config;
