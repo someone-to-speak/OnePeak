a12397a137c2b02444ca461599d252b7c3f14a80
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-11-05 15:24:41 +0900

    style: 헤더, 네브바 숨길 경로 지정

--- src/app/(mobile)/layout.tsx
+++ src/app/(mobile)/layout.tsx
@@ -1,20 +1,31 @@
+"use client";
+
 import Header from "@/components/header/Header";
 import "../globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Navibar from "@/components/nav/Navibar";
+import { usePathname } from "next/navigation";
 
 export default function MobileLayout({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+  // 디버깅을 위해 pathname 값 확인
+  console.log("Current pathname:", pathname);
+
+  // pathname이 null이 아니면 특정 경로에서 헤더와 푸터 숨기기
+  // const hideHeaderFooter = pathname !== null && ["/loginInfo/*"].includes(pathname);
+  const hideHeaderFooter = pathname !== null && pathname.startsWith("/loginInfo");
+
   return (
     <div>
       <div className="w-full min-w-[320px] max-w-[600px] mx-auto my-0  min-h-full">
-        <Header />
+        {!hideHeaderFooter && <Header />}
         {children}
-        <Navibar />
+        {!hideHeaderFooter && <Navibar />}
       </div>
     </div>
   );
