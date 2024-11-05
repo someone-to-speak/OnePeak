3dff2295bba19da65ae4a085dd31625146f9d5b9
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-11-06 03:13:45 +0900

    fix: 헤더에 푸터와 같은 z-index 속성 추가

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -16,7 +16,7 @@ const Header = () => {
   };
 
   return (
-    <header className="w-full sticky min-w-[320px] max-w-[600px] top-0 left-0 right-0 mx-auto mt-0">
+    <header className="w-full sticky min-w-[320px] max-w-[600px] top-0 left-0 right-0 mx-auto mt-0 z-[200]">
       <div className="flex items-center justify-between box-border px-4 h-[52px] bg-blue-200">
         <div className="flex justify-center items-center w-[28px] h-[28px] bg-white">로고</div>
         <div className="flex justify-center items-center ml-auto gap-2">
