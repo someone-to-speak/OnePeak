da96b9067ea74b63736496710375997c472cc1ce
Seoyun Kim <seoyunkim@Seoyunui-MacBookPro.local>
2024-11-07 12:59:10 +0900

    addbigicon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,13 +12,13 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/app-icon.png",
-        sizes: "156x156",
+        src: "/app-icon-192.png",
+        sizes: "192x192",
         type: "image/png"
       },
       {
-        src: "/app-icon.png",
-        sizes: "156x156",
+        src: "/app-icon-512.png",
+        sizes: "512x512",
         type: "image/png"
       }
     ]
