274fe39b720167194b9cbf00161394ba631bef9f
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-11-14 09:45:35 +0900

    ㄴ단어/문법 오답노트 모바일버전 상단탭 배경 width 고정

--- src/components/wrongAnswer/GrammarList.tsx
+++ src/components/wrongAnswer/GrammarList.tsx
@@ -48,7 +48,7 @@ const GrammarList = ({ userId }: { userId: string }) => {
 
   return (
     <div className="flex flex-col gap-4 md:gap-[30px] md:px-3">
-      <div className="bg-gray-900 flex rounded-[22px] w-full h-[46px] p-2.5 justify-center items-center md:bg-transparent md:gap-5">
+      <div className="bg-gray-900 flex rounded-[22px] w-[343px] mx-auto h-[46px] p-2.5 justify-center items-center md:bg-transparent md:gap-5 ">
         <button
           className={`${
             isReviewed === "미완료"
