ff51740f7438c54f208bfe499f044a7e64093000
mingjeongg <alswjd6141@naver.com>
2024-11-14 12:24:42 +0900

    style: 문의하기페이지, 신고하기페이지 모바일/pc 디자인시안 수정 반영 완료

--- src/components/chat/report.tsx
+++ src/components/chat/report.tsx
@@ -10,29 +10,38 @@ const Report = () => {
   };
 
   return (
-    <div className="w-full p-[30px] gap-[20px] flex flex-col  rounded-[16px] bg-[#fcfcfc] ">
-      <div className="flex gap-[4px] justify-center items-center ">
-        <Image src={reportIcon} alt="신고하기 아이콘" />
-        <h1 className="text-black text-xl font-bold font-['SUIT']">신고하기</h1>
-      </div>
-      <div className="flex flex-col gap-[6px]">
-        <h3 className="text-[#0c0c0c] text-sm font-bold font-['SUIT']">사진첨부</h3>
-        <input type="file" onChange={handleImgFile} multiple />
-      </div>
-      <div className=" flex flex-col gap-[6px]">
-        <h3 className="text-[#0c0c0c] text-sm font-bold font-['SUIT'] ">신고사유</h3>
-        <textarea
-          placeholder="사유를 작성해 주세요"
-          className="p-[20px] flex items-start border border-[#d9d9d9] rounded-[10px] bg-[#FDFDFD]   "
-        ></textarea>
-      </div>
-      <div className=" flex gap-[10px] ">
-        <button className="w-full p-[10px]  rounded-[10px] bg-[#D9D9D9]  text-center text-[#fcfcfc] text-base font-medium font-['SUIT']   ">
-          취소
-        </button>
-        <button className=" w-full p-[10px]  rounded-[10px] bg-[#F50000]  text-center text-[#fcfcfc] text-base font-medium font-['SUIT']   ">
-          신고
-        </button>
+    <div className="w-full px-[16px]  flex flex-col   bg-[#FFF] ">
+      <div className="flex gap-[6px] md:mt-[70px]  ">
+        <Image src={reportIcon} alt="신고하기 아이콘" className="md:hidden" />
+        <h1
+          className="text-[#0c0c0c] text-lg font-bold font-['SUIT']
+       md:text-[#0c0c0c] md:text-4xl md:font-bold md:font-['SUIT'] "
+        >
+          신고하기
+        </h1>
+      </div>{" "}
+      <div className="md:max-w-[454px] md:flex md:flex-col md:mx-auto ">
+        <div className="flex flex-col gap-[28px] mt-[105px] mb-[238px] ">
+          <div className="flex flex-col gap-[6px]">
+            <h3 className="text-[#0c0c0c] text-lg font-bold font-['SUIT']">사진첨부</h3>
+            <input type="file" onChange={handleImgFile} multiple />
+          </div>
+          <div className=" flex flex-col gap-[6px]">
+            <h3 className="text-[#0c0c0c] text-lg font-bold font-['SUIT']'] ">신고사유</h3>
+            <textarea
+              placeholder="사유를 작성해 주세요"
+              className="p-[20px] flex items-start border border-[#d9d9d9] rounded-[10px] bg-[#FDFDFD]   "
+            ></textarea>
+          </div>
+        </div>
+        <div className=" flex gap-[10px] mb-[56px] ">
+          <button className="w-full p-[10px]  rounded-[10px] bg-[#D9D9D9]  text-center text-[#fcfcfc] text-base font-medium font-['SUIT']   ">
+            취소
+          </button>
+          <button className=" w-full p-[10px]  rounded-[10px] bg-[#F50000]  text-center text-[#fcfcfc] text-base font-medium font-['SUIT']   ">
+            신고
+          </button>
+        </div>
       </div>
     </div>
   );
