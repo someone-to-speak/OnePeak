e02d5b765ca0e364eec4a302d968d3f53fa4768b
byoungyup_kim <qudduq999@daum.net>
2024-11-15 21:29:10 +0900

    feat: 환경마다 로그인 작업

--- src/services/supabaseAuth.ts
+++ src/services/supabaseAuth.ts
@@ -1,7 +1,12 @@
 import { createClient } from "@/utils/supabase/client";
 import { Provider } from "@supabase/supabase-js";
 
-const redirectTo = process.env.NODE_ENV === "production" ? "https://onepeak.vercel.app" : "http://localhost:3000";
+const redirectTo =
+  process.env.NODE_ENV === "production"
+    ? process.env.VERCEL_GIT_COMMIT_REF === "main"
+      ? "https://onepeak.vercel.app" // main 브랜치
+      : "https://one-peak-dev.vercel.app" // dev 브랜치
+    : "http://localhost:3000"; // 개발 환경
 
 export const signInWithProvider = async (provider: Provider) => {
   const supabase = createClient();
