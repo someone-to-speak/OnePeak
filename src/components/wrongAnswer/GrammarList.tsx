0f94f40c9c641f138c0bb53704e3f32df303f3ae
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-11-19 10:11:20 +0900

    style: 오답노트 탭 패딩 수정

--- src/components/wrongAnswer/GrammarList.tsx
+++ src/components/wrongAnswer/GrammarList.tsx
@@ -35,7 +35,7 @@ const GrammarList = ({ userId }: { userId: string }) => {
 
   return (
     <div className="flex flex-col gap-4 md:gap-[30px] md:px-3">
-      <div className="bg-gray-900 flex rounded-[22px] w-[343px] mx-auto md:ml-1 h-[46px] p-2.5 justify-center items-center md:justify-start md:bg-transparent md:gap-[10px]">
+      <div className="bg-gray-900 flex rounded-[22px] w-[343px] mx-auto md:ml-1 h-[46px] p-1 justify-center items-center md:justify-start md:bg-transparent md:gap-[10px]">
         <button
           className={`${
             isReviewed === "미완료"
